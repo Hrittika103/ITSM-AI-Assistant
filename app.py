1b074e0b3feed129d8f8cc490f76e49b2a913be5
Hrittika103 <72061129+Hrittika103@users.noreply.github.com>
2026-04-08 18:52:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+
+
+import os
+import sqlite3
+import pandas as pd
+
+# Check if DB exists
+if not os.path.exists("tickets.db"):
+    # Load Excel and create DB dynamically
+    df = pd.read_excel("itsm_tickets_dataset_v2_1000_records.xlsx")
+    conn = sqlite3.connect("tickets.db")
+    df.to_sql("tickets", conn, index=False, if_exists="replace")
+    conn.close()
 import streamlit as st
 from ai_engine import ask_ai
 
